65300835dc3040ff8c147e8570cb0acf6f8fd0db
Abdul Hamid <hamidkhattak469@gmail.com>
2025-01-05 16:59:23 +0500

    Creating Main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,40 @@
+import ChatbotIcon from "./components/ChatbotIcon";
+
 const App = () => {
   return (
-    <div>App</div>
+    <div className="container">
+      <div className="chatbot-popup">
+        {/* Chatbot Header */}
+        <div className="chatbot-header">
+          <div className="header-info">
+            <ChatbotIcon />
+            <h2 className="logo-text">Chatbot</h2>
+          </div>
+          <button className="material-symbols-rounded"> keyboard_arrow_down </button>
+        </div>
+
+        {/* Chatbot Body */}
+        <div className="chat-body">
+          <div className="message bot-message">
+            <ChatbotIcon />
+            <p className="message-text"> Hey there, <br /> How can I help you today? </p>
+          </div>
+
+          <div className="message user-message">
+            <ChatbotIcon />
+            <p className="message-text"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+          </div>
+        </div>
+
+        {/* Chatbot Footer */}
+        <div className="chat-footer">
+          <form action="#" className="chat-form">
+            <input type="text" placeholder="Message..." className="message-input" required />
+            <button className="material-symbols-rounded"> arrow_upward </button>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
