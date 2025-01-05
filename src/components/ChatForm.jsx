c82c143933733090abd3a1d4a03ba28d8d6792a1
Abdul Hamid <hamidkhattak469@gmail.com>
2025-01-05 21:03:07 +0500

    Create ChatForm component and move chat form from App component to ChatForm component

---
+++ src/components/ChatForm.jsx
@@ -0,0 +1,10 @@
+const ChatForm = () => {
+    return (
+        <form action="#" className="chat-form">
+            <input type="text" placeholder="Message..." className="message-input" required />
+            <button className="material-symbols-rounded"> arrow_upward </button>
+        </form>
+    )
+}
+
+export default ChatForm;
